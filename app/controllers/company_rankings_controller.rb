5cc168e4f9ac9c8f4c443a5de1a97d7bb7f09d6d
Chris Patuzzo <chris@patuzzo.co.uk>
2019-07-31 16:01:38 +0100

    Pass either an Outcome or a Group to CompanyRanking

--- app/controllers/company_rankings_controller.rb
+++ app/controllers/company_rankings_controller.rb
@@ -1,13 +1,13 @@
 class CompanyRankingsController < ApplicationController
-  ID_FORMAT = /(\d{4})-outcome-(\d+)/
+  ID_FORMAT = /(\d{4})-(outcome|group)-(\d+)/
 
   before_action do # TODO: add the rack-cors gem?
     headers['Access-Control-Allow-Origin'] = '*'
   end
 
   def show
-    outcome = Outcome.find(outcome_id)
-    ranking = CompanyRanking.new(outcome, year)
+    rankable = rankable_class.find(rankable_id)
+    ranking = CompanyRanking.new(rankable, year)
     presenter = CompanyRankingPresenter.new(ranking)
 
     render json: presenter
@@ -19,7 +19,11 @@ class CompanyRankingsController < ApplicationController
     params.fetch(:id)[ID_FORMAT, 1]
   end
 
-  def outcome_id
-    params.fetch(:id)[ID_FORMAT, 2]
+  def rankable_class
+    params.fetch(:id)[ID_FORMAT, 2].capitalize.constantize
+  end
+
+  def rankable_id
+    params.fetch(:id)[ID_FORMAT, 3]
   end
 end
