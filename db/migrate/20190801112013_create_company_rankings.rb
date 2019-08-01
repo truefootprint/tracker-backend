ecaca78a5ad348a7f64f27befb98e25fd6e6f9c6
Chris Patuzzo <chris@patuzzo.co.uk>
2019-08-01 15:09:43 +0100

    Ensure company_rankings has rows for all outcome/company/year
    If a company hasn’t answered any of the questions
    that contribute to an outcome/group, we still want
    there to be a row in the table that contains null
    values.

--- db/migrate/20190801112013_create_company_rankings.rb
+++ db/migrate/20190801112013_create_company_rankings.rb
@@ -3,42 +3,34 @@ class CreateCompanyRankings < ActiveRecord::Migration[5.2]
     execute <<-SQL
       create materialized view if not exists company_rankings as (
         with recursive r(rankable_type, rankable_id, company_id, year, value, count, rank) as (
-          select
-            'Outcome',
-            outcome_id,
-            company_id,
-            year,
-            value,
-            cast(1 as bigint),
-            rank() over(
+          select 'Outcome', o.id, c.id, y, value,
+            case when ov is null then cast(0 as bigint) else cast(1 as bigint) end,
+
+            case when ov is null then null else rank() over(
               partition by outcome_id, year
               order by case when o.higher_is_better then -value else value end
-            )
+            ) end
 
-          from outcome_values ov
-          join outcomes o on ov.outcome_id = o.id
+          from outcomes o
+          cross join companies c
+          cross join (select distinct year as y from outcome_values) _
+          left join outcome_values ov
+            on ov.company_id = c.id
+            and ov.outcome_id = o.id
+            and year = y
 
           union all
 
-          select
-            'Group',
-            group_id,
-            company_id,
-            year,
-            avg_rank,
-            count,
+          select 'Group', group_id, company_id, year, avg_rank, count,
             case when cast(count as float) / size < 0.5 then null else rank() over(
               partition by group_id, year
               order by case when cast(count as float) / size < 0.5 then null else avg_rank end
             ) end
 
           from (
-            select distinct
-              group_id,
-              company_id,
-              year,
-              avg(r.rank) over(partition by group_id, company_id, year) as avg_rank,
-              count(1)    over(partition by group_id, company_id, year) as count
+            select distinct group_id, company_id, year,
+              avg(r.rank)    over(partition by group_id, company_id, year) as avg_rank,
+              count(r.value) over(partition by group_id, company_id, year) as count
 
             from groups g
             join group_members gm on gm.group_id = g.id
