c9da976318502be17207d6e9244ba4c7d5d3022f
Chris Patuzzo <chris@patuzzo.co.uk>
2019-08-02 16:54:25 +0100

    Allow fields to be fetched for ancestry members

--- app/presenters/ancestry_presenter.rb
+++ app/presenters/ancestry_presenter.rb
@@ -1,11 +1,12 @@
 class AncestryPresenter
   ALL = %i[parents ancestors children descendents]
 
-  attr_accessor :ancestry, :relations
+  attr_accessor :ancestry, :relations, :fields
 
-  def initialize(ancestry, relations)
+  def initialize(ancestry, relations, fields = %i[name])
     self.ancestry = ancestry
     self.relations = Array(relations)
+    self.fields = fields
   end
 
   def as_json(_options = {})
@@ -16,6 +17,25 @@ class AncestryPresenter
     hash[:children] = ancestry.children if relations.include?(:children)
     hash[:descendents] = ancestry.descendents if relations.include?(:descendents)
 
-    hash
+    hash.merge(attributes: attributes)
+  end
+
+  private
+
+  def attributes
+    members_by_type.each.with_object({}) do |(type, members), types|
+      types[type] = type.constantize
+        .where(id: member_ids(members))
+        .pluck(:id, *fields)
+        .each.with_object({}) { |(id, *v), ids| ids[id] = fields.zip(v).to_h }
+    end
+  end
+
+  def members_by_type
+    ancestry.seen.group_by { |m| m.fetch(:type) }
+  end
+
+  def member_ids(members)
+    members.map { |m| m.fetch(:id) }
   end
 end
