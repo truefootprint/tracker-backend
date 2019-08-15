dc6d8fd83847350a9df3439278b8a5957a7257d6
Chris Patuzzo <chris@patuzzo.co.uk>
2019-08-15 14:03:50 +0100

    Use a points system instead of average rankings when ranking companies

--- app/queries/company_rankings_query.rb
+++ app/queries/company_rankings_query.rb
@@ -29,10 +29,10 @@ class CompanyRankingsQuery
 
         union all
 
-        select 'Group', group_id, company_id, sector_id, year, avg_rank, null, threshold, count,
+        select 'Group', group_id, company_id, sector_id, year, points, null, threshold, count,
           case when cast(count as float) / size < threshold then null else rank() over(
             partition by group_id, sector_id, year, threshold
-            order by case when cast(count as float) / size < threshold then null else avg_rank end
+            order by case when cast(count as float) / size < threshold then null else -points end
           ) end,
 
           count(case when cast(count as float) / size < threshold then null else 1 end) over(
@@ -41,12 +41,20 @@ class CompanyRankingsQuery
 
         from (
           select distinct group_id, company_id, sector_id, year, threshold,
-            avg(r.rank)   over(partition by group_id, company_id, sector_id, year, threshold) as avg_rank,
+
+            case when gm.member_type = 'Group' then
+              sum(r.value) over(partition by group_id, company_id, sector_id, year, threshold)
+            else
+              sum(coalesce(rp.points, 0)) over(partition by group_id, company_id, sector_id, year, threshold)
+            end as points,
+
             count(r.rank) over(partition by group_id, company_id, sector_id, year, threshold) as count
 
           from groups g
           join group_members gm on gm.group_id = g.id
           join r on r.rankable_type = gm.member_type and r.rankable_id = gm.member_id
+
+          left join rank_points rp on rp.rank = r.rank
         ) _
         join group_sizes gs on gs.id = group_id
       ),
@@ -55,6 +63,14 @@ class CompanyRankingsQuery
       ),
       completeness (threshold) as (
         values (0.1), (0.2), (0.3), (0.4), (0.5), (0.6), (0.7), (0.8), (0.9), (1)
+      ),
+      rank_points (rank, points) as (
+        values (1, 100), (2, 70), (3, 50), (4, 40), (5, 35), (6, 32), (7, 30),
+               (8, 29), (9, 28), (10, 27), (11, 26), (12, 25), (13, 24), (14, 23),
+               (15, 22), (16, 21), (17, 20), (18, 19), (19, 18), (20, 17), (21, 16),
+               (22, 15), (23, 14), (24, 13), (25, 12), (26, 11), (27, 10), (28, 10),
+               (29, 10), (30, 10), (31, 10), (32, 10), (33, 10), (34, 10), (35, 10),
+               (36, 10), (37, 10), (38, 10), (39, 10), (40, 10)
       )
       select * from r
     SQL
