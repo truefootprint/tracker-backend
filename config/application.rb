bcf64990253e7e86066e2b2c59e561aeb7e51404
Chris Patuzzo <chris@patuzzo.co.uk>
2019-08-16 13:51:05 +0100

    Rename tracker-v2 -> tracker-backend

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TrackerV2
+module TrackerBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
