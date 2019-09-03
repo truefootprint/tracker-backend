10ceaf9253b2cc5632c5027b840c990ef6af1d22
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-03 16:50:20 +0100

    Set the page references for Gold Fields

--- app/models/page_reference.rb
+++ app/models/page_reference.rb
@@ -2,18 +2,31 @@ class PageReference < ApplicationRecord
   belongs_to :answer
 
   def self.seed
-    company = Company.find_by!(name: "Glencore")
-    url = "https://www.glencore.com/dam:jcr/633f190c-76d6-42b3-beca-debb25134556/2018-Glencore-Sustainability-Report_.pdf"
+    company = Company.find_by!(name: "Gold Fields")
+    url = "https://www.goldfields.com/pdf/investors/integrated-annual-reports/2018/iar-2018.pdf"
 
     references = [
-      ["Total energy use", 10],
+      [91,  "Total revenue (in million $)"],
+      [133, "Total production (in tonnes)"],
+      [99,  "Total GHG emissions (scope 1  and 2)"],
+      [133, "Total energy use"],
+      [133, "Total water use"],
+      [6,   "Percentage of water recycled"],
+      [133, "Significant environmental incidents (spills)"],
+      [133, "Lost time injury frequency rate (LTIFR)"],
+      [133, "Number of employee fatalities"],
+      [78,  "Entry level wage as a percentage of local minimum wage"],
+      [94,  "Percentage of in-country procurement"],
+      [78,  "Gender pay gap"],
     ]
 
-    references.each do |question_text, page|
+    references.each do |page, question_text|
       question = Question.find_by!(text: question_text)
-      answer = Answer.find_by!(question: question, company: company)
+      answers = Answer.where(question: question, company: company)
 
-      PageReference.create!(answer: answer, url: url, page: page)
+      answers.each do |answer|
+        PageReference.create!(answer: answer, url: url, page: page)
+      end
     end
   end
 end
