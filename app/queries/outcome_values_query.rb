4124301d1c86002c9ba35255358751edf5060b7d
Chris Patuzzo <chris@patuzzo.co.uk>
2019-11-20 10:18:33 +0000

    Don’t divide by 1 when a divisor exists but the answer is null

--- app/queries/outcome_values_query.rb
+++ app/queries/outcome_values_query.rb
@@ -5,31 +5,36 @@ class OutcomeValuesQuery
 
   def to_sql
     <<~SQL
-      select
-        o.id as outcome_id,
-        c1.id as company_id,
-        a1.year,
-        a1.value / coalesce(a2.value, 1) as value,
+      select * from (
+        select
+          o.id as outcome_id,
+          c1.id as company_id,
+          a1.year,
 
-        case when a2 is null then a1.auditor_id else
-          case when a1.auditor_id is not null and a2.auditor_id is not null
-          then a1.auditor_id
-          else null end
-        end as auditor_id
+          case when m.divisor_id is null
+            then a1.value else a1.value / a2.value
+          end as value,
 
-      from outcomes o
-      join mappings m on m.outcome_id = o.id
+          case when a2 is null then a1.auditor_id else
+            case when a1.auditor_id is not null and a2.auditor_id is not null
+            then a1.auditor_id
+            else null end
+          end as auditor_id
 
-      join questions q1 on m.question_id = q1.id
-      join answers a1 on a1.question_id = q1.id
-      join companies c1 on a1.company_id = c1.id
+        from outcomes o
+        join mappings m on m.outcome_id = o.id
 
-      left join questions q2 on m.divisor_id = q2.id
-      left join answers a2 on a2.question_id = q2.id
-      left join companies c2 on a2.company_id = c2.id
+        join questions q1 on m.question_id = q1.id
+        join answers a1 on a1.question_id = q1.id
+        join companies c1 on a1.company_id = c1.id
 
-      where c1.id = c2.id and a1.year = a2.year and a2.value != 0
-      or c2.id is null
+        left join questions q2 on m.divisor_id = q2.id
+        left join answers a2 on a2.question_id = q2.id
+        left join companies c2 on a2.company_id = c2.id
+
+        where c1.id = c2.id and a1.year = a2.year and a2.value != 0
+        or c2.id is null
+      ) _ where value is not null
     SQL
   end
 end
